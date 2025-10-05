18cba2dcb35bde4efa718554515ddda9758fa471
Aditya Raj <adityarajp1208@gmail.com>
2025-10-05 15:45:43 +0530

    update profile endpoint added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,13 +1,13 @@
-import express, { Router } from "express"
-import { signup, login, logout } from "../controllers/auth.controller.js"
-
+import express, { Router } from "express";
+import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
-router.post("/signup", signup)
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-
-router.post("/login",login);
-
-router.post("/logout",logout);
+router.put("/update-profile",protectRoute, updateProfile);
+router.get("/checking",protectRoute, (req,res) => res.status(200).json(req.user));
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cookieParser from "cookie-parser"
 import { ENV } from "./lib/env.js"
 import path from "path"
 
@@ -13,6 +14,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 
 app.use("/api/auth",authRoutes);
